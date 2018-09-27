850bc2e0de7864fe92bbea8ae25272692c048a62
nikunjjhaveri123 <nikunjjhaveri@gmail.com>
2018-09-27 17:05:14 -0400

    added file for song list

--- SongLib/src/application/Controller.java
+++ SongLib/src/application/Controller.java
@@ -18,5 +18,34 @@ public class Controller {
 	public void setMainStage(Stage stage) {
 		mainStage = stage;
 	}
+	
+}
+
+class Song {
+	String name;
+	String artist;
+	String album;
+	Integer year; 
+	public Song(String name, String artist, String album, Integer year)
+	{
+		this.name = name;
+		this.artist = artist; 
+		this.album = album;
+		this.year = year;
+	}
+	public Song(String name, String artist) {
+		this(name, artist, "", null);
+	}
+	public Song(String name, String artist, String album) {
+		this(name, artist, album, null);
+	}
+	public Song(String name, String artist, Integer year) {
+		this(name, artist, "", year);
+	}
+	public boolean equals(Song one)
+	{
+		return false;
+	}
 
 }
+
