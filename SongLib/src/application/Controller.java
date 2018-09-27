a81c501b34f5b572957f9192957603cb9f9c51e3
nikunjjhaveri123 <nikunjjhaveri@gmail.com>
2018-09-27 19:24:40 -0400

    worked on text file reader

--- SongLib/src/application/Controller.java
+++ SongLib/src/application/Controller.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.*;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -9,24 +12,60 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Controller {
 	
 	Stage mainStage;
 	
+	final static String fileName = "songlist.txt";
+	
 	public void setMainStage(Stage stage) {
 		mainStage = stage;
 	}
 	
+	//Reads a file and returns and ObservableList of Songs that can be used in the listview
+	public ObservableList<Song> fileRead(String fileName)
+	{
+		ObservableList<Song> listOfSongs = FXCollections.observableArrayList();
+		try {
+			FileReader fr = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(fr);
+	
+			String line;
+			while(br.readLine()!=null)
+			{
+				line = br.readLine();
+				String name = "";
+				String artist = "";
+				String year = "";
+				String album = "";
+				StringTokenizer str = new StringTokenizer(line, "/");
+				name = str.nextToken();
+				artist = str.nextToken();
+				album = str.nextToken();
+				year = str.nextToken();
+				listOfSongs.add(new Song(name, artist, album, year));
+				
+			}
+	
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return listOfSongs;
+	}
 }
 
 class Song {
 	String name;
 	String artist;
 	String album;
-	Integer year; 
-	public Song(String name, String artist, String album, Integer year)
+	String year; 
+	public Song(String name, String artist, String album, String year)
 	{
 		this.name = name;
 		this.artist = artist; 
@@ -34,17 +73,23 @@ class Song {
 		this.year = year;
 	}
 	public Song(String name, String artist) {
-		this(name, artist, "", null);
+		this(name, artist, "", "");
 	}
+	/*
 	public Song(String name, String artist, String album) {
-		this(name, artist, album, null);
+		this(name, artist, album, "");
 	}
-	public Song(String name, String artist, Integer year) {
+	public Song(String name, String artist, String year) {
 		this(name, artist, "", year);
+		
 	}
+	*/
 	public boolean equals(Song one)
 	{
 		return false;
+	}
+	public String toString() {
+		return name + " by " + artist;
 	}
 
 }
